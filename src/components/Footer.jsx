8242a31e15a51b35b8543394b59215e6170a3743
portable-ewaade <portableewaade@gmail.com>
2023-08-13 21:27:24 +0100

    review

---
+++ src/components/Footer.jsx
@@ -0,0 +1,110 @@
+import React from 'react'
+import { BsInstagram, BsLinkedin, BsTwitter } from 'react-icons/bs'
+
+const Footer = () => {
+  return (
+    <section className=''  style={{
+        backgroundImage: "url('../../../assets/footer-bg.png')",
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center' }}>
+
+        <div className='pt-5 '>
+        <div className="container ">
+            <div className="row mt-3 ">
+                <div className="col-md-3">
+                    <ul> <h5> Company</h5></ul>
+                    <div className="text-color">
+                    <a href="/">
+                        <ul> About</ul>
+                    </a>
+                    <a href="/">
+                        <ul>Our Services</ul>
+                    </a>
+                    <a href="/">
+                        <ul>Gallery</ul>
+                    </a>
+                    </div>                  
+                </div>
+                <hr className='d-md-none mt-4'/>
+
+                <div className="col-md-3">
+                <ul> <h5>Students</h5></ul>
+                <div className="text-color">
+                <a href="/">
+                        <ul>Student Reviews</ul>
+                    </a>
+                    <a href="/">
+                        <ul>Get Started</ul>
+                    </a>
+                </div>
+                    
+                </div>
+                <hr className='d-md-none mt-4'/>
+
+                <div className="col-md-3">
+                <ul> <h5>Explore</h5></ul>
+                <div className="text-color">
+                <a href="/">
+                        <ul>Tech News</ul>
+                    </a>
+                    <a href="/">
+                        <ul>Tech Careers</ul>
+                    </a>
+                    <a href="/">
+                        <ul>Courses </ul>
+                    </a>
+                </div>
+                   
+                </div>
+                <hr className='d-md-none mt-4'/>
+
+                <div className="col-md-3">
+                <ul> <h5>Legal & Help</h5></ul>
+                <div className="text-color">
+                <a href="/">
+                        <ul>Privacy Policy</ul>
+                    </a>
+                    <a href="/">
+                        <ul>Terms & Conditions</ul>
+                    </a>
+                    <a href="/">
+                        <ul>Contact Us</ul>
+                    </a>
+                </div>
+                    
+                </div>
+                <div className="col-md-11 ms-md-4">
+                    <hr />
+                </div>
+            </div>
+        </div>
+    
+            <div className="container text-center pt-4 pb-5">
+                <img src="../../../assets/logo.png" alt="..." className='img-fluid mb-1' style={{width: "8rem", height: "auto"}}/>
+                <h6 className='letter-space mb-2'> CODE GEEKS 9JA</h6>
+                <p className='fs-small'>Copyright &copy; 2023, Code Geeks 9ja. All rights reserverd. </p>
+
+                <div>
+                    <a href="/" className='mx-3'>
+                    < BsInstagram />
+                    </a>
+                    <a href="/">
+                    < BsTwitter />
+                    </a>
+                    <a href="/" className='mx-3'>
+                    < BsLinkedin />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+   
+  )
+}
+
+export default Footer
+
+{/* <a href="#" class="position-absolute position-fixed bottom-0 end-0 top-25 m-4 arrow-up">
+<i class="bi bi-arrow-up-circle h2"></i>
+</a> */}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <section className='mb-4'>
+     <div className='container'>
+     <nav className="navbar navbar-expand-lg pt-2">
+     <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+            <img src="../../../assets/logo.png" alt="..." className='img-fluid me-4'
+            style={{width:"6rem", height:"3rem"}}/>
+        </a>
+        <button className="navbar-toggler bg-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+        </button>   
+     </div>
+
+     <div className="collapse navbar-collapse mt-4 me-5" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto fs-normal ms-4 ms-md-0 fw-medium ">
+            <li className="nav-item me-4 ">
+            <Link href="/" class="nav-link text-white active font-weight-600"> HOME
+            </Link>
+            </li>
+            <li className="nav-item me-4 ">
+            <Link href="/" class="nav-link text-white active"> ABOUT
+            </Link>
+            </li>
+            <li className="nav-item me-4 ">
+            <Link href="/" class="nav-link text-white active"> REVIEW
+            </Link>
+            </li>
+            <li className="nav-item me-4 ">
+            <Link href="/" class="nav-link text-white active"> CONTACT
+            </Link>
+            </li>
+
+           
+        </ul>
+        <button className="btnn " type="submit">
+            <a href="/">Get Started </a>
+            </button>
+            
+        </div>
+    </nav>
+    </div>
+   </section>
+  )
+}
+
+export default Navbar
